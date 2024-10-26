7b2651c315ef176c66e1b9993400dd160615c969
krzysckh <kpm@linux.pl>
2024-10-26 11:47:15 +0200

    map-{width,height} → map-size, mouse-based steering

--- lager.scm
+++ lager.scm
@@ -2,30 +2,28 @@
  (owl toplevel)
  (raylib))
 
-(define *window-width* 600)
-(define *window-height* 600)
+(define *window-size* 600)
 
-(define *cam-offset* (list (/ *window-width* 2) (/ *window-height* 2)))
+(define *cam-offset* (list (/ *window-size* 2) (/ *window-size* 2)))
 
 (define *grid-size* 50)
 (define *point-radius* 10)
-(define *frames-per-pos* 5)
+(define *frames-per-pos* 8)
 
-(define *speed-base* 2)
+(define *speed-base* 5)
 (define *map-mult* 2)
-(define *map-size* (list (* *window-width* 2) (* *window-height* 2)))
+(define *map-size* (* *window-size* 2))
 
 (define *n-points* 128)
 
-(define dgl:min (- 0 (car *map-size*)))
-(define dgl:max (car *map-size*))
+(define dgl:min (- 0 *map-size*))
+(define dgl:max *map-size*)
 (define (draw-grid)
   (for-each
    (λ (v)
      (draw-line-simple dgl:min v dgl:max v white)
      (draw-line-simple v dgl:min v dgl:max white))
    (iota dgl:min *grid-size* dgl:max))) ;; assuming width == height
-
 
 (define (maybe-next-mail)
   (let ((envelope (check-mail)))
@@ -39,18 +37,18 @@
 
 (define (remap-point-to-map map-box pt)
   (list
-   (remap (car pt)  (- 0 (car *map-size*))  (car *map-size*)  (car map-box)  (+ (car map-box) map:sz))
-   (remap (cadr pt) (- 0 (cadr *map-size*)) (cadr *map-size*) (cadr map-box) (+ (cadr map-box) map:sz))))
+   (remap (car pt)  (- 0 *map-size*)  *map-size*  (car map-box)  (+ (car map-box) map:sz))
+   (remap (cadr pt) (- 0 *map-size*) *map-size* (cadr map-box) (+ (cadr map-box) map:sz))))
 
-(define (draw-map pos points)
-  (let* ((map-box (list (- *window-width* map:pad map:sz) map:pad map:sz map:sz))
+(define (draw-map pos points size)
+  (let* ((map-box (list (- *window-size* map:pad map:sz) map:pad map:sz map:sz))
          (user (remap-point-to-map map-box pos)))
     (draw-rectangle map-box (make-color 255 255 255 128))
     (draw-rectangle-lines map-box 4 black)
     (for-each
      (λ (pt) (draw-circle (remap-point-to-map map-box pt) 2 green))
      points)
-    (draw-circle user 5 red)
+    (draw-circle user (max 1 (round (remap size 0 (* 2 *map-size*) 0 map:sz))) red)
     ))
 
 ;; rs min max → rs (x y)
@@ -69,8 +67,8 @@
 
 ;; meant to be ran over the network
 (define (decider)
-  (lets ((min (- 0 (car *map-size*)))
-         (max (cadr *map-size*))
+  (lets ((min (- 0 *map-size*))
+         (max *map-size*)
          (points rs (random-points
                      (seed->rands (time-ms))
                      *n-points* min max
@@ -108,10 +106,9 @@
 
 (define (main)
   (thread 'decider (decider))
-  (next-thread)
   (set-target-fps! 60)
   (with-window
-   *window-width* *window-height* "*lager*"
+   *window-size* *window-size* "*lager*"
    (let loop ((x 0)
               (y 0)
               (points #n)
@@ -120,15 +117,19 @@
               (speed-mult 2.0)
               (frame-ctr 0)
               )
-     (let* ((mailq (let loop ((acc #n))
+     (lets ((mailq (let loop ((acc #n))
                      (lets ((_ v (maybe-next-mail)))
                        (if v
                            (loop (append acc (list v)))
                            acc))))
-            (x (if (key-down? key-a) (- x (* speed-mult *speed-base*)) x))
-            (x (if (key-down? key-d) (+ x (* speed-mult *speed-base*)) x))
-            (y (if (key-down? key-w) (- y (* speed-mult *speed-base*)) y))
-            (y (if (key-down? key-s) (+ y (* speed-mult *speed-base*)) y))
+            ;; (x (if (key-down? key-a) (- x (* speed-mult *speed-base*)) x))
+            ;; (x (if (key-down? key-d) (+ x (* speed-mult *speed-base*)) x))
+            ;; (y (if (key-down? key-w) (- y (* speed-mult *speed-base*)) y))
+            ;; (y (if (key-down? key-s) (+ y (* speed-mult *speed-base*)) y))
+            (mp (map (λ (v) (remap v 0 *window-size* -1 1)) (mouse-pos)))
+            (x y (values
+                  (+ x (* speed-mult *speed-base* (car mp)))
+                  (+ y (* speed-mult *speed-base* (cadr mp)))))
             (pos (list x y))
             (camera (list *cam-offset* pos 0 zoom))
             ;; update data based on mailq
@@ -137,8 +138,12 @@
             (size (+ size (len (mesgof 'enlarge! mailq))))
             )
 
-       (when (= frame-ctr 0)
+       ;; ask the decider to update location every n frames or if i think a a point was touched
+       (when (or (any (λ (pt) (collision-circles? pt *point-radius* pos (+ 10 size))) points) (= frame-ctr 0))
          (mail 'decider (tuple 'update-pos! pos size)))
+
+       (when (key-down? key-equal)
+         (mail 'threadmain (tuple 'enlarge!)))
 
        (draw
         (clear-background gray)
@@ -149,7 +154,7 @@
            (for-each (λ (pos) (draw-circle pos *point-radius* green)) points)
            (draw-circle pos (+ 10 size) red)
            ))
-        (draw-map pos points)
+        (draw-map pos points size)
         (draw-fps 0 0)
         )
        (if (window-should-close?)
