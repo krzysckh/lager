46de619c4d4d7c54f2849e1955da97e986bb2997
krzysckh <krzysckh@wp.pl>
2024-11-01 16:35:12 +0100

    test more shit

--- lager-server.scm
+++ lager-server.scm
@@ -4,13 +4,21 @@
  (lager const)
  (lager common))
 
+;; (define (get-block fd len)
+;;   (let ((bv (try-get-block fd len #f)))
+;;     (if (= (bytevector-length bv) len)
+;;         bv
+;;         (bytevector-append bv (get-block fd (- len (bytevector-length bv)))))))
+
 (define (make-client fd ip)
   (let ((thrname (string->symbol (str "client@" ip "-" (time-ns)))))
     (thread
      thrname
      (let loop ()
        (when (readable? fd)
-         (let* ((size (u16->n (bytevector->list (try-get-block fd 2 #t))))
+         (let* ((bv (try-get-block fd 2 #t))
+                (_ (print bv))
+                (size (u16->n (bytevector->list bv)))
                 (res (reintern (fasl-decode (bytevector->list (try-get-block fd size #t)) (tuple 'bad)))))
            (tuple-case res
              ((bad)
@@ -28,6 +36,7 @@
          (let ((fasl (fasl-encode v)))
            (write-bytes fd (n->u16 (len fasl)))
            (write-bytes fd fasl)))
+
        (loop)))))
 
 (λ (_)
