ef1f0930fa0e0d57f3465b7199a193d7290fc4ca
krzysckh <krzysckh@wp.pl>
2024-11-01 11:35:53 +0100

    fix _quickbuild

--- _quickbuild.sh
+++ _quickbuild.sh
@@ -6,4 +6,4 @@ wget -O raymath.h https://raw.githubusercontent.com/raysan5/raylib/f1007554a0a81
 wget -O raylib.h https://raw.githubusercontent.com/raysan5/raylib/f1007554a0a8145060797c0aa8169bdaf2c1c6b8/src/raylib.h
 wget -O libraylib.a https://pub.krzysckh.org/raylib-bin/libraylib5-linux64-opengl33.a
 
-cc -o lager-quickbuild -I. lager.c -L. -l:libraylib5-linux64-opengl33.a -lm
+cc -o lager-quickbuild -I. lager.c -L. -l:libraylib.a -lm
